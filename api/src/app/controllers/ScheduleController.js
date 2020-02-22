52e16db63465fd1fb570474dcc0ea4eab6cf2079
leticialmb <leticiabarreto196@gmail.com>
2020-02-22 11:20:56 -0500

    Criação de pasta api e gobarberweb

---
+++ api/src/app/controllers/ScheduleController.js
@@ -0,0 +1,49 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+
+class ScheduleController {
+	async listSchedule(req, res) {
+		// check if user is provider or not
+		const checkProvider = await User.findOne({
+			where: {
+				id: req.userId,
+				provider: true
+			}
+		});
+
+		if (!checkProvider) {
+			return res.status(401).json({ error: 'User is not a provider' });
+		}
+
+		// list by actual date
+		const { date } = req.query;
+		const parseDate = parseISO(date);
+
+		console.log(req.userId);
+
+		const appointments = await Appointment.findAll({
+			where: {
+				providers_id: req.userId,
+				calceled_at: null,
+				date: {
+					[Op.between]: [startOfDay(parseDate), endOfDay(parseDate)]
+				}
+			},
+			include: [
+				{
+					model: User,
+					as: 'user',
+					attributes: ['name']
+				}
+			],
+			order: ['date']
+		});
+
+		return res.json(appointments);
+	}
+}
+
+export default new ScheduleController();
---
+++ api/src/app/controllers/SessionController.js
@@ -0,0 +1,59 @@
+import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
+
+import User from '../models/User';
+import authConfig from '../../config/auth';
+import File from '../models/File';
+
+class SessionController {
+	//verifica se usuario e senha estão corretos
+	async store(req, res) {
+		const schema = Yup.object().shape({
+			email: Yup.string()
+				.email()
+				.required(),
+			password: Yup.string().required()
+		});
+
+		if (!(await schema.isValid(req.body))) {
+			return res.status(400).json({ error: 'Validation fails.' });
+		}
+
+		const { email, password } = req.body;
+		const user = await User.findOne({
+			where: { email },
+			include: [
+				{
+					model: File,
+					as: 'avatar',
+					attributes: ['id', 'path', 'url']
+				}
+			]
+		});
+
+		if (!user) {
+			return res.status(401).json({ error: 'User not found.' });
+		}
+
+		if (!(await user.checkPassword(password))) {
+			return res.status(401).json({ error: 'Invalid password' });
+		}
+
+		const { id, name, avatar, provider } = user;
+
+		return res.json({
+			user: {
+				id,
+				name,
+				email,
+				avatar,
+				provider
+			},
+			token: jwt.sign({ id }, authConfig.secret, {
+				expiresIn: authConfig.expiresIn
+			})
+		});
+	}
+}
+
+export default new SessionController();
